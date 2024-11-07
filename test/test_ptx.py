d2a49299759d9f5567ce57c6f065684ae1c56097
judy <mesozoic.egg@proton.mail>
2024-11-07 16:07:37 +0800

    test

--- test/test_ptx.py
+++ test/test_ptx.py
@@ -1,9 +1,10 @@
 import os
-os.environ["PTX"] = "1"
 from tinygrad import Tensor, Device
 from tinygrad.ops import Ops, UOp, Variable, sym_infer, sint
 from tinygrad.engine.realize import get_kernel
 from tinygrad.renderer.ptx import PTXRenderer
+from tinygrad.renderer import Renderer
+from tinygrad.renderer.cstyle import ClangRenderer, CUDARenderer
 
 def schedule(a: Tensor):
   scheduled, vars = a.schedule_with_vars() 
@@ -11,23 +12,20 @@ def schedule(a: Tensor):
     if si.ast.op is Ops.SINK:
       return si.ast
   
-def render(ast: UOp, PTX_CSTYLE: bool=False):
-  kernel = get_kernel(Device["CUDA"].renderer, ast)
+def render(ast: UOp, renderer: Renderer):
+  kernel = get_kernel(renderer, ast)
   kernel.linearize()
-  renderer = PTXRenderer("sm_86") if PTX_CSTYLE else PTXRenderer("sm_86")
   src = renderer.render("rendered", kernel.uops)
   return src
 
 def compare_ptx(a: Tensor):
   ast = schedule(a)
-  src0 = render(ast)
-  src1 = render(ast, PTX_CSTYLE=True)
-  if src0 != src0:
-    print("src0")
-    print(src0)
-    print("src1")
-    print(src1)
-  assert src0 == src1
+  src0 = render(ast, PTXRenderer("sm_86"))
+  print(src0)
+  src1 = render(ast, CUDARenderer("sm_86"))
+  print(src1)
+  clang = render(ast, ClangRenderer())
+  print(clang)
 
     
 
