d7183da32ba1b99831970265701aba040e430608
judy <mesozoic.egg@proton.mail>
2024-11-08 19:29:20 +0800

    squash! wip

--- tinygrad/renderer/ptx2.py
+++ tinygrad/renderer/ptx2.py
@@ -1,6 +1,15 @@
 from tinygrad.renderer.cstyle import CStyleLanguage
-from tinygrad.renderer.ptx import ptx_matcher
-from tinygrad.dtype import PtrDType
+from tinygrad.renderer.ptx import ptx_matcher, render_val
+from tinygrad.dtype import PtrDType, dtypes
+from tinygrad.ops import GroupOp, UnaryOps, BinaryOps, TernaryOps, Ops, UOp, PatternMatcher, UPat, cast_float_to_bf16
+
+def render_const(ctx, x):
+  val = render_val(x, dtypes.uint32)
+  return f"mov.b32"
+
+base = PatternMatcher([
+  (UPat(Ops.CONST, dtype=dtypes.uint32, name="x"), render_const),
+])
 
 class PTXRenderer(CStyleLanguage):
   kernel_prefix = """.version VERSION
@@ -9,7 +18,9 @@ class PTXRenderer(CStyleLanguage):
 .visible .entry"""
 
   extra_matcher = ptx_matcher
+  string_rewrite = base
   def render_kernel(self, function_name, kernel, bufs, regs) -> str:
+    print(kernel)
     kernel = [f".reg .{reg};" for reg in regs] + kernel + ["ret;"]
     def fmt(line): return line if line[0]=="$" else "\t" + line.replace(" ", "\t" if len(line.split(" ")[0]) > 7 else "\t\t", 1)
     return (f"{self.kernel_prefix} {function_name}(\n\t" +
