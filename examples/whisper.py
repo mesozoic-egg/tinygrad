99cd34a4804b6dd8de8b2ecf4f961222bd30b693
Mesozoic Egg <mesozoic.egg@proton.mail>
2025-01-07 15:39:21 +0800

    suppress nonspeech token

--- examples/whisper.py
+++ examples/whisper.py
@@ -1,6 +1,6 @@
 # thanks to https://github.com/openai/whisper for a good chunk of MIT licensed code
 
-import sys, base64, multiprocessing, itertools, collections, zlib, datetime
+import sys, base64, multiprocessing, itertools, collections, zlib, datetime, math
 from typing import Optional, Union, Literal, List
 
 from tinygrad import Tensor, TinyJit, nn
@@ -292,12 +292,19 @@ def timestamp_filter(logits: Tensor):
   logits = Tensor(logits)
   return logits
 
+@TinyJit
+def non_speech_filter(logits: Tensor):
+  tokens = Tensor([1, 2, 7, 8, 9, 10, 14, 25, 26, 27, 28, 29, 31, 58, 59, 60, 61, 62, 63, 90, 91, 92, 93, 357, 366, 438, 532, 685, 705, 796, 930, 1058, 1220, 1267, 1279, 1303, 1343, 1377, 1391, 1635, 1782, 1875, 2162, 2361, 2488, 3467, 4008, 4211, 4600, 4808, 5299, 5855, 6329, 7203, 9609, 9959, 10563, 10786, 11420, 11709, 11907, 13163, 13697, 13700, 14808, 15306, 16410, 16791, 17992, 19203, 19510, 20724, 22305, 22935, 27007, 30109, 30420, 33409, 34949, 40283, 40493, 40549, 47282, 49146, 50358, 50357, 50257, 50360, 50359, 50361])
+  logits[:, tokens] = -math.inf
+  
+
 def inferloop(model, ctx: np.ndarray, encoded_audio: Tensor, temperature: int, num_sample: int, eot: int):
   pos, next_tokens = 0, ctx
   sum_probs = Tensor.zeros(ctx.shape[0])
   for i in (_trange:=trange(num_sample)):
     logits = model.decoder(Tensor(next_tokens), pos, encoded_audio)[:, -1].contiguous()
-    # logits = timestamp_filter(logits)
+    non_speech_filter(logits)
+    logits = timestamp_filter(logits)
     if temperature == 0:
       next_tokens = argmax_sampling(logits)
     else:
@@ -320,7 +327,7 @@ def transcribe_waveform(model: Whisper, enc, waveforms, output_fh, truncate=Fals
     language_token = enc._special_tokens["<|startoftranscript|>"] + 1 + tuple(LANGUAGES.keys()).index("en")
     start_tokens.append(language_token)
     start_tokens.append(enc._special_tokens["<|transcribe|>"])
-  start_tokens.append(enc._special_tokens["<|notimestamps|>"])
+  # start_tokens.append(enc._special_tokens["<|notimestamps|>"])
   eot = enc._special_tokens["<|endoftext|>"]
   def gettexttoks(line):
     return [tok for tok in line if tok < eot or tok > enc._special_tokens["<|notimestamps|>"]][-nsample+len(start_tokens):]
