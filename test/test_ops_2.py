40397b668e4798c16c6624a19ccfae57321e30d8
root <root@localhost>
2025-07-16 23:14:01 +0800

    set up failed test_abs

--- test/test_ops_2.py
+++ test/test_ops_2.py
@@ -1,4 +1,4 @@
-import time, math, unittest, functools, os
+import time, math, unittest, functools, os, torch
 import numpy as np
 from typing import List, Callable
 import warnings
@@ -6,6 +6,46 @@ from tinygrad.helpers import DISABLE_COMPILER_CACHE, getenv, IMAGE, DEBUG, CI, C
 from tinygrad import Tensor, Device, dtypes
 from tinygrad.tensor import _to_np_dtype
 from tinygrad.device import is_dtype_supported
+
+def helper_test_op(shps, torch_fxn, tinygrad_fxn=None, atol=1e-6, rtol=1e-3, grad_atol=1e-4, grad_rtol=1e-3,
+                   forward_only=False, vals=None, low=-2, high=2):
+  if tinygrad_fxn is None: tinygrad_fxn = torch_fxn
+  ts, tst = prepare_test_op(low, high, shps, vals, forward_only)
+
+  st = time.monotonic()
+  out = torch_fxn(*ts)
+  torch_fp = time.monotonic() - st
+
+  st = time.monotonic()
+  ret = tinygrad_fxn(*tst).realize()
+  tinygrad_fp = time.monotonic() - st
+
+  def compare(s, tinygrad_output, torch_output, atol, rtol):
+    try:
+      if np.issubdtype(tinygrad_output.dtype, np.floating):
+        np.testing.assert_allclose(tinygrad_output, torch_output, atol=atol, rtol=rtol)
+      else:
+        np.testing.assert_equal(tinygrad_output, torch_output)
+    except Exception as e:
+      raise Exception(f"{s} failed shape {tinygrad_output.shape}: {e}")
+
+  compare("forward pass", ret.numpy(), out.detach().cpu().numpy(), atol=atol, rtol=rtol)
+
+  torch_fbp, tinygrad_fbp = np.nan, np.nan
+
+def prepare_test_op(low, high, shps, vals, forward_only=False):
+  if shps is None:
+    ts = [torch.tensor(x, requires_grad=(not forward_only)) for x in vals]
+  else:
+    np.random.seed(0)
+    np_data = [np.random.uniform(low=low, high=high, size=size).astype(_to_np_dtype(dtypes.default_float)) for size in shps]
+    ts = [torch.tensor(data, requires_grad=(not forward_only)) for data in np_data]
+  for i in range(len(ts)):
+    # NOTE: torch default int64 for python ints input
+    if ts[i].dtype == torch.int64: ts[i] = ts[i].type(torch.int32)
+  tst = [Tensor(x.detach().cpu().numpy(), requires_grad=(not forward_only)) for x in ts]
+  return ts, tst
+
 
 class TestOps(unittest.TestCase):
   def test_full(self):
@@ -45,13 +85,14 @@ class TestOps(unittest.TestCase):
     print(grid_x.numpy())
     #print(grid_y.numpy())
 
-  @unittest.skip("")
   def test_arange(self):
     print(Tensor.arange(100).numpy())
 
-  @unittest.skip("")
   def test_linespace(self):
     print(Tensor.linspace(5, 10, 3).numpy())
+
+  def test_abs(self):
+    helper_test_op([(45,65)], torch.abs, Tensor.abs)
 
   @unittest.skip("")
   def test_sum(self):
