2d90abb6d97aeafb244c9c5ed85411173e85213f
root <root@localhost>
2025-07-24 10:46:55 +0800

    more test_abs

--- test/test_ops_2.py
+++ test/test_ops_2.py
@@ -126,8 +126,8 @@ class TestOps(unittest.TestCase):
 
   def test_abs(self):
     with Context(NOOPT=1): helper_test_op([(2,2)], torch.abs, Tensor.abs)
-    #with Context(NOOPT=0): helper_test_op([(8,8)], torch.abs, Tensor.abs)
-    #helper_test_op([(45,65)], torch.abs, Tensor.abs)
+    with Context(NOOPT=0): helper_test_op([(8,8)], torch.abs, Tensor.abs)
+    helper_test_op([(45,65)], torch.abs, Tensor.abs)
 
   def _test_abs(self, data, dtype):
     a = Tensor(data,  dtype=dtype, device="asm") 
