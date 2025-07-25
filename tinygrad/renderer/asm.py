126c62823a9527375d56cd76e8238af9318cc655
root <root@localhost>
2025-07-25 14:07:44 +0800

    float_cmp uses assign_multiple

--- tinygrad/renderer/asm.py
+++ tinygrad/renderer/asm.py
@@ -580,18 +580,26 @@ def to_bool(ctx, x, a):
 def float_cmp(ctx, x, a, b):
   if dtypes.is_int(a.dtype) or dtypes.is_bool(a.dtype):
     reg_type = IReg
-    dst = ctx.r.assign(x, reg_type=IReg)
-    exclude_dst = [dst] if reg_type == IReg else []
-    src_a = ctx.r.assign(a, reg_type=reg_type, excludes=exclude_dst)
-    src_b = ctx.r.assign(b, excludes=[src_a] + exclude_dst, reg_type=reg_type)
+    if False:
+      dst = ctx.r.assign(x, reg_type=IReg)
+      exclude_dst = [dst]
+      src_a = ctx.r.assign(a, reg_type=reg_type, excludes=exclude_dst)
+      src_b = ctx.r.assign(b, excludes=[src_a] + exclude_dst, reg_type=reg_type)
+    else:
+      regs = ctx.r.assign_multiple([x, a, b], IReg)
+      dst, src_a, src_b = tuple(regs)
     temp_regs = ctx.r.alloc_multiple(2, IReg, [src_a, src_b, dst])
-    temp_reg, temp_reg_2 = temp_regs[0], temp_regs[1]
+    temp_reg, temp_reg_2 = tuple(temp_regs)
   else:
     reg_type = FReg
-    dst = ctx.r.assign(x, reg_type=IReg)
-    exclude_dst = [dst] if reg_type == IReg else []
-    src_a = ctx.r.assign(a, reg_type=reg_type, excludes=exclude_dst)
-    src_b = ctx.r.assign(b, excludes=[src_a] + exclude_dst, reg_type=reg_type)
+    if False:
+      dst = ctx.r.assign(x, reg_type=IReg)
+      exclude_dst = []
+      src_a = ctx.r.assign(a, reg_type=reg_type, excludes=exclude_dst)
+      src_b = ctx.r.assign(b, excludes=[src_a] + exclude_dst, reg_type=reg_type)
+    else:
+      dst = ctx.r.assign(x, IReg)
+      src_a, src_b = tuple(ctx.r.assign_multiple([a, b], FReg))
     temp_reg = ctx.r.alloc(FReg, [src_a, src_b, dst])
     temp_reg_2 = ctx.r.alloc(IReg, [src_a, src_b, dst])
   ctx.r.return_reg(temp_reg)
