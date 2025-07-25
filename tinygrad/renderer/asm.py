8cd33ea8706036c3badc4f2831754c9a1f70c0fe
root <root@localhost>
2025-07-25 14:00:08 +0800

    to_bool uses assign_multiple, cont'd

--- tinygrad/renderer/asm.py
+++ tinygrad/renderer/asm.py
@@ -539,17 +539,21 @@ def to_bool(ctx, x, a):
     else:
       regs = ctx.r.assign_multiple([x, a], reg_type=IReg)
       dst, src = regs[0], regs[1]
-    print(f"{regs=}")
-    print()
     temp_reg = ctx.r.alloc(excludes=regs, reg_type=reg_type)
     ctx.r.return_reg(temp_reg)
   else:
     reg_type = FReg
-    dst = ctx.r.assign(x, reg_type=IReg)
-    exclude_dst_reg = [dst] if reg_type == IReg else []
-    src = ctx.r.assign(a, reg_type=reg_type, excludes=exclude_dst_reg)
-    temp_reg = ctx.r.alloc(excludes=[src]+exclude_dst_reg, reg_type=reg_type)
-    ctx.r.return_reg(temp_reg)
+    if False:
+      dst = ctx.r.assign(x, reg_type=IReg)
+      exclude_dst_reg = [dst] if reg_type == IReg else []
+      src = ctx.r.assign(a, reg_type=reg_type, excludes=exclude_dst_reg)
+      temp_reg = ctx.r.alloc(excludes=[src]+exclude_dst_reg, reg_type=reg_type)
+      ctx.r.return_reg(temp_reg)
+    else:
+      dst = ctx.r.assign(x, IReg)
+      src = ctx.r.assign(a, FReg)
+      temp_reg = ctx.r.alloc(FReg, [src])
+      ctx.r.return_reg(temp_reg)
   if Arch.arm:
     if dtypes.is_int(a.dtype):
       cmp = f"cmp {src}, #0"
